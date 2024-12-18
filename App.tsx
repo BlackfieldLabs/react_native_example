27cc1c2b30fbf671faed7b03df96b1fb9fd6ac72
Tamara Stojkovic <tamaras@veriests.com>
2024-12-18 16:29:47 +0100

    [Android] - Whisper library
    + Added whisper library to the code and imported it.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,34 @@
 import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { initWhisper } from 'whisper.rn'
 
 const App = () => {
+    console.log("Tamara: App start.");
+    console.log("Tamara: Lalalalala.");
+    const [transcription, setTranscription] = useState<string>(''); // Holds the transcribed text
+    const [isListening, setIsListening] = useState<boolean>(false); // Tracks whether Whisper is listening
+    const [whisperContext, setWhisperContext] = useState<any>(null);
   const [recognizedText, setRecognizedText] = useState<string>(''); // State to hold recognized text
+    let result;
+  const startListening = async () => {
+      console.log("Tamara: start listening!");
+      try{
+          console.log("Tamara: try block!");
+          // Initialize Whisper with the model file
+                      const whisperContext = await initWhisper({
+                        filePath: 'file://.../ggml-tiny.en.bin',
+                      })
+      }
+      catch(e:Exception) {
+          // Displays error thrown by the try block
+          result = e.Message;
+          console.log("Tamara: catch block!", e.Message);
+      }
+      finally {
+          // Runs irrespective of try and catch blocks
+          console.log("Tamara: finally block!");
+      }
+  }
 
   const handleButtonPress = () => {
     // For now, simulate setting the recognized text
@@ -15,7 +41,7 @@ const App = () => {
       <Text style={styles.title}>Start Voice Recognition</Text>
 
       {/* Button */}
-      <Button title="Start" onPress={handleButtonPress} />
+      <Button title="Start" onPress={startListening} />
 
       {/* TextView to display recognized text */}
       <Text style={styles.result}>{recognizedText}</Text>
